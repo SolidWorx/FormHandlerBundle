ea11ac4f3bc9bae379a55df527dff243caa36c7a
Pierre du Plessis <pierre@pcservice.co.za>
2023-03-23 09:06:34 +0200

    Add ReturnTypeWillChange attribute to Options::offsetGet

--- src/Options.php
+++ src/Options.php
@@ -69,6 +69,7 @@ class Options implements \ArrayAccess, \Countable, \IteratorAggregate
         return array_key_exists($offset, $this->options);
     }
 
+    #[\ReturnTypeWillChange]
     /**
      * {@inheritdoc}
      *
